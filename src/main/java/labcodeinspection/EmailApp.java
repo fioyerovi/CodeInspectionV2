573e6fc4708e107abcb3dbf0836a90d51093718e
Fiorella Yerovi N <fioreyerovi@gmail.com>
2022-06-13 17:01:10 -0500

    Me salen 18 violaciones, deberían salir 35...

--- src/main/java/labcodeinspection/EmailApp.java
+++ src/main/java/labcodeinspection/EmailApp.java
@@ -1,9 +1,8 @@
 package labcodeinspection;
 
 import java.util.Scanner;
-@SuppressWarnings("PMD.UseUtilityClass")
 public class EmailApp {
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
